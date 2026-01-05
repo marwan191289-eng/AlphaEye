687cf086d9a03117f36811fb875655b9f7604bc6
{username} <marwan191289@gmail.com>
2026-01-05 10:19:53 +0400

    Merge remote-tracking branch 'origin/main'

--- index.tsx
+++ index.tsx
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
 // Entry point for the frontend app
 
-=======
->>>>>>> 6466315e1e273bc05b5afc7f3b80cf4ac718d9d3
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-<<<<<<< HEAD
 const rootElement = document.getElementById('root');
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
@@ -22,10 +18,3 @@ if (rootElement) {
 }
 
 <script type="module" src="/index.tsx"></script>
-=======
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
->>>>>>> 6466315e1e273bc05b5afc7f3b80cf4ac718d9d3
